f3e907dbd5e7b2cf5c69b7a8c01fed882a8942ce
Cassava <kidndumbi@gmail.com>
2018-03-02 01:13:25 -0500

    remove dots

--- src/app/shared/projectList.ts
+++ src/app/shared/projectList.ts
@@ -1,6 +1,24 @@
 export const _PROJECTLIST = [
-    { name: "Spotify music Search", route: "/shopifySearch", description: "App for searching music on Spotify using Spotify API", likes: 0 },
-    { name: "Chatter", route: "/chatter", description: "Reall Anonymous time chat App", likes: 0  },
-    { name: "roll Dice", route: "/rollDice", description:"A dual dice rolling App", likes:0 }
+    {
+        name: "Spotify music Search", route: "/shopifySearch",
+        description: "App for searching music on Spotify using Spotify API",
+        likes: 0,
+        active:false,
+        color:"orange"
+    },
+    {
+        name: "Chatter", route: "/chatter",
+        description: "Reall Anonymous time chat App",
+        likes: 0,
+        active:false,
+        color:"sky-blue"
+    },
+    {
+        name: "roll Dice", route: "/rollDice",
+        description: "A dual dice rolling App",
+        likes: 0,
+        active:true,
+        color:"purple"
+    }
 
 ]
