06715d11106df1f74140c399def021c78c69e988
Cassava <kidndumbi@gmail.com>
2018-03-02 00:14:11 -0500

    remove unneeded menu items

--- src/app/shared/projectList.ts
+++ src/app/shared/projectList.ts
@@ -1,6 +1,6 @@
 export const _PROJECTLIST = [
     { name: "Spotify music Search", route: "/shopifySearch", description: "App for searching music on Spotify using Spotify API", likes: 0 },
     { name: "Chatter", route: "/chatter", description: "Reall Anonymous time chat App", likes: 0  },
-    { name: "roll Dice", route: "/rollDice", description:"A dual dice rolling App", likes:0 },
-    { name: "Project Select", route: "projectSelect", description: "", likes: 0  }
+    { name: "roll Dice", route: "/rollDice", description:"A dual dice rolling App", likes:0 }
+
 ]
