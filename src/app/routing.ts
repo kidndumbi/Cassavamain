7ba3705996e14134fcdf36a1352551e088186c5d
Cassava <kidndumbi@gmail.com>
2018-03-19 23:26:36 -0400

    fix issue with routing errror

--- src/app/routing.ts
+++ src/app/routing.ts
@@ -28,7 +28,6 @@ export const appRoutes: Routes = [
         path: 'rollDice',
         component: RollDiceComponent
     },
-    ,
     {
         path: 'knowledge',
         component: KnowledgeSourcesComponent
